29b5f9c106c69b13fd3682e111566cce3d6c6eb3
Maria Helena Varela <helena.varelakg@gmail.com>
2025-03-13 14:23:37 -0300

    update and delete tasks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
 import time
+from operations import * 
 
 
 def main():
@@ -16,71 +17,20 @@ def main():
 
         # Crud
         if option == '1':
-            def create_tasks(tasks):
-                name_task = input("What's the name of your task? ")
-                desc_task = input("What's the description of your task? ")
-                
-                dic = { "name": name_task,
-                       "description" : desc_task
-                    }
-                
-                try:
-                    with open("tasks.json","r") as file: # abre o tasks.json no modo leitura
-                        tasks = json.load(file) # armazena na variavel tasks o json
-                except (FileNotFoundError, json.JSONDecodeError): # se o json nao existir ou for corrompido
-                    tasks = [] # tasks é inicializada como uma lista vazia
-                tasks.append(dic) # adiciona dic à tasks
-
-                with open("tasks.json","w") as file: # abre o arquivo no modo escrita 
-                        json.dump(tasks, file, indent = 4)
-
-                print("Your task has been added!")
             create_tasks(tasks) 
         
         # cRud
         if option == '2':
-            def read_tasks(tasks):
-                try:
-                    with open("tasks.json", "r") as file:
-                        tasks = json.load(file)
-                except (FileNotFoundError, json.JSONDecodeError):
-                    print("You don't have tasks yet.")
-
-                index = 0
-                for t in tasks:
-                    print(f"{str(index)}. {t['name']}: {t['description']}")
-                    index += 1
-
             read_tasks(tasks)
             
         # crUd
-        if option == '3': 
-            if not tasks:
-                print("You don't have tasks yet!")
-            else:
-                read_task(tasks)
-                ind = int(input("Which the position of the task you want to update? "))
-                if ind >= 0 and ind < len(tasks):
-                    new_name = input("What's your new task? ")
-                    new_desc = input("What's your new description? ")
-                    update_task(tasks, ind, new_name, new_desc)
-                    print('Your task has been updated!')
-                else: 
-                    print(f"Choose a number between 0 and {len(tasks) - 1}")
-
+        if option == '3':
+            update_tasks(tasks)
+            
         # cruD
         if option == '4':
-            if not tasks:
-                print("You don't have tasks yet!")
-            else:
-                read_task(tasks)
-                num_task = int(input("Which the position of the task you want to delete? "))
-                if num_task >= 0 and num_task < len(tasks):
-                    delete_task(tasks, num_task)
-                    print('Your task has been deleted!')
-                else: 
-                    print(f"Choose a number between 0 and {len(tasks) - 1}")
-            
+            delete_tasks(tasks)
+
         if option == '5':
             print("Exiting")
             break
--- operations.py
+++ operations.py
@@ -1,22 +1,75 @@
-from models import Task
+import json
 
-def create_task(tasks, add_name, add_desc):
-    tarefa = Task(name = add_name,description = add_desc)
-    tasks.append(tarefa)
+def create_tasks(tasks):
+    name_task = input("What's the name of your task? ")
+    desc_task = input("What's the description of your task? ")
+    dic = { "name": name_task,
+            "description" : desc_task
+            }
+    try:
+        with open("tasks.json","r") as file: # abre o tasks.json no modo leitura
+            tasks = json.load(file) # armazena na variavel tasks o json
+    except (FileNotFoundError, json.JSONDecodeError): # se o json nao existir ou for corrompido
+        tasks = [] # tasks é inicializada como uma lista vazia
+    
+    tasks.append(dic) # adiciona dic à tasks
+    print("Your task has been added!")
+    with open("tasks.json","w") as file: # abre o arquivo no modo escrita 
+        json.dump(tasks, file, indent = 4)
 
-def read_task(tasks):
-    if not tasks:
+
+def read_tasks(tasks):
+    try:
+        with open("tasks.json", "r") as file:
+            tasks = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
         print("You don't have tasks yet!")
-    else: 
-        tar = 0
-        for t in tasks:
-            print(f"{str(tar)}. {str(t)}")
-            tar += 1
+    index = 0
+    for t in tasks:
+        print(f"{str(index)}. {t['name']}: {t['description']}")
+        index += 1
 
-def update_task(tasks, ind, new_name, new_desc):
-    old_task = tasks[ind]
-    old_task.name = new_name
-    old_task.description = new_desc
+def update_tasks(tasks):
+    try:
+        with open("tasks.json","r") as file:
+            tasks = json.load(file)
+        read_tasks(tasks)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("You don't have tasks yet!")
+    else:
+        try:
+            ind = int(input("Which the position of the task you want to update? "))
+            if ind >= 0 and ind < len(tasks):
+                new_name = input("What's your new task? ")
+                new_desc = input("What's your new description? ")
+                old_task = tasks[ind]
+                old_task["name"] = new_name
+                old_task["description"] = new_desc
+                with open("tasks.json","w") as file:
+                    json.dump (tasks, file, indent=4)
+                print('Your task has been updated!')
+            else: 
+                print(f"Choose a number between 0 and {len(tasks) - 1}")
+        except ValueError:
+            print("Enter a number, please")
+        
 
-def delete_task(tasks, num_task):
-    tasks.pop(num_task)
+def delete_tasks(tasks):
+    try:
+        with open("tasks.json","r") as file:
+            tasks = json.load(file)
+        read_tasks(tasks)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("You don't have tasks yet!")
+    else:
+        try:   
+            num_task = int(input("Which the position of the task you want to delete? "))
+            if num_task >= 0 and num_task < len(tasks):
+                tasks.pop(num_task)
+                with open("tasks.json","w") as file:
+                    json.dump(tasks, file, indent=4)
+                print('Your task has been deleted!')
+            else: 
+                print(f"Choose a number between 0 and {len(tasks) - 1}")
+        except ValueError:
+            print("Enter a number, please")
